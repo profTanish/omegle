a559be7bbbe983c9da748d48060f8bc5f5ddfb6c
profTanish <profile.tanish@gmail.com>
2025-03-16 15:45:02 +0530

    Done with webrtc

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,7 +7,6 @@ function App() {
   return (
     <BrowserRouter>
        <Routes>
-         <Route path="/room" element={<Room />} />
          <Route path="/" element={<Landing />} />
        </Routes>
      </BrowserRouter>
